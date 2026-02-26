3229aca751f8ed48584fbcf0c21b9867fab75ed7
xyc0833 <2031362123@qq.com>
2026-02-26 13:07:50 +0800

    使用注解的方式 实现aop

--- src/main/java/com/book/config/MainConfiguration.java
+++ src/main/java/com/book/config/MainConfiguration.java
@@ -4,11 +4,15 @@ import com.book.entity.ArtTeacher;
 import com.book.entity.Student;
 import com.book.entity.Teacher;
 import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
 @EnableAspectJAutoProxy
 //表示这个类是一个 配置类
 @Configuration
-@ComponentScan("com.book.entity")
+@ComponentScans({
+    @ComponentScan("com.book.entity")
+})
+
 public class MainConfiguration {
 
 }
